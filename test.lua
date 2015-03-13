c8efa5cda4b9c050ffc029b19fb83de6b35faccb
Robert Nelson <robert@rnelson.ca>
2015-03-13 09:26:14 -0400

    Added button test

--- test.lua
+++ test.lua
@@ -28,3 +28,42 @@ pause()
 led.setblue(0)
 
 print("LED Test Complete.")
+
+-- Second test, Buttons
+
+print("Beginning Button Test")
+
+bttn = {}
+oldb = -1
+b = -1
+print("Push each button once")
+for i=1,6 do
+    while oldb == b do
+        b = button.wait()
+    end
+    oldb = b
+
+    if bttn[b] == nil then
+        bttn[b] = 1
+    else
+        bttn[b] = bttn[b] + 1
+    end
+end
+
+c = 0
+for i=1,6 do
+    if bttn[i] then
+        c = c + 1
+        print(string.format("Button %d pushed %d times.", i, bttn[i]))
+    else
+        print(string.format("Button %d was not pushed.", i))
+    end
+end
+
+if c == 6 then
+    print("Button test PASSED")
+end
+
+print("End Button Test")
+
+os.exit()
